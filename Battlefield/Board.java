c4a4fd08487b828df7c0b0ec7e2891b3290d789f
Leo is awesome <lc599@drexel.edu>
2014-12-22 17:29:12 -0500

    can play more than 1 game at a time

--- Battlefield/Board.java
+++ Battlefield/Board.java
@@ -4,57 +4,84 @@ import java.lang.reflect.Constructor;
 import java.io.*;
 
 public class Board {
-	public static void main(String[] args) throws Exception {
-        BoardState board = new BoardState();
-        board.showPositions();
 
+    private static int whiteWins = 0;
+    private static int blackWins = 0;
+    private static int ties = 0;
+
+	public static void main(String[] args) throws Exception {
+        System.out.println("");
+
+        int plays = Integer.parseInt(args[2]);
         System.out.println("Player 1: " + args[0]);
         System.out.println("Player 2: " + args[1]);
 
-        Class<?> clazz = Class.forName(args[0]);
-        Class<?> clazz2 = Class.forName(args[1]);
-        Constructor<?> ctor = clazz.getConstructors()[0];
-        Constructor<?> ctor2 = clazz2.getConstructors()[0];
-        Player p1 = (Player) ctor.newInstance(new Object[]{Player.Color.White});
-        Player p2 = (Player) ctor2.newInstance(new Object[]{Player.Color.Black});
+        int play = 0;
+        while (play++ < plays){
+            System.out.println("\nGame " + play + ":");
 
-        Move playerMove;
+            BoardState board = new BoardState();
+            board.showPositions();
+
+            Class<?> clazz = Class.forName(args[0]);
+            Class<?> clazz2 = Class.forName(args[1]);
+            Constructor<?> ctor = clazz.getConstructors()[0];
+            Constructor<?> ctor2 = clazz2.getConstructors()[0];
+            Player p1 = (Player) ctor.newInstance(new Object[]{Player.Color.White});
+            Player p2 = (Player) ctor2.newInstance(new Object[]{Player.Color.Black});
+
+            Move playerMove;
+
+            System.out.println("______________\n");
+            while(true){
+                System.out.println("Turn: " + (board.getMoveCount()+1));
+
+                playerMove= p1.getMove(board);
+                System.out.println(playerMove.toString() + "\n");
+                board = board.makeMove(playerMove);
+                board.display();
+                if (isGameOver(board)){
+                    break;
+                }
+
+                System.out.println("______________\n");
+                System.out.println("Turn: " + (board.getMoveCount()+1));
+
+                playerMove = p2.getMove(board);
+                System.out.println(playerMove.toString() + "\n");
+                board = board.makeMove(playerMove);
+                board.display();
+                if (isGameOver(board)){
+                    break;
+                }
+
+                System.out.println("______________\n");
+            }
+        }
 
         System.out.println("______________\n");
-        while(true){
-            System.out.println("Turn: " + (board.getMoveCount()+1));
+        System.out.println("Results after " + plays + " plays:");
+        System.out.println(args[0] + " wins: " + whiteWins);
+        System.out.println(args[1] + " wins: " + blackWins);
+        System.out.println("Ties: " + ties);
 
-            playerMove= p1.getMove(board);
-            System.out.println(playerMove.toString() + "\n");
-            board = board.makeMove(playerMove);
-            board.display();
-            if (isGameOver(board)){
-                break;
-            }
-
-            System.out.println("______________\n");
-            System.out.println("Turn: " + (board.getMoveCount()+1));
-
-            playerMove = p2.getMove(board);
-            System.out.println(playerMove.toString() + "\n");
-            board = board.makeMove(playerMove);
-            board.display();
-            if (isGameOver(board)){
-                break;
-            }
-
-            System.out.println("______________\n");
-        }
+        System.out.println("\nClosing engine ... ");
 	}
 
     private static boolean isGameOver(BoardState board){
         if (board.gameOver()){
-            if (board.getWinner() == Player.Color.White)
-                System.out.println("White wins");
-            else if (board.getWinner() == Player.Color.Black)
-                System.out.println("Black wins");
-            else if (board.getWinner() == null)
-                System.out.println("Is tie");
+            if (board.getWinner() == Player.Color.White){
+                System.out.println("\nResult: White wins");
+                whiteWins++;
+            }
+            else if (board.getWinner() == Player.Color.Black){
+                blackWins++;
+                System.out.println("\nResult: Black wins");
+            }
+            else if (board.getWinner() == null){
+                ties++;
+                System.out.println("\nResult: tie");
+            }
             return true;
         }
 
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Chess Engine and Players
-## Summary
 A mediocre Java chess engine that can make players compete against each other.
 ## Setup
 1) After cloning the repository and changing into the directory, create the jar files with
@@ -12,9 +11,14 @@ $ just test
 ```
 3) Add a `player.java` file into the `Battlefield` directory to be able to play it against a `RandomPlayer`. Have it play against a RandomPlayer with
 ```sh
-$ just battle [PlayerName]
+$ just battle PlayerName
 # For example, to have the FirstOrderPlayer go against a RandomPlayer
 # $ just battle FirstOrderPlayer
+```
+To have the engine play `N` games, use
+```sh
+$ just battleN N PlayerName
+# Where N is an integer
 ```
 4) Remove all the class and jar files if necessary
 ```sh
